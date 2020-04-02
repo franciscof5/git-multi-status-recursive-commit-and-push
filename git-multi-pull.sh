ce34fb57b88991b6f85cdffe243d2f3cc25deafe
Francisco Mat <francisco@f5sites.com>
2020-04-02 05:01:47 -0300

    update echo colors

--- git-multi-pull.sh
+++ git-multi-pull.sh
@@ -3,9 +3,10 @@ while read -r line
 do
     if [ -d "$line/.git" ]
 	then
-		cd $line
+		echo $line
 		echo -en "\033[0;35m"
-		echo "### cd $line && git pull"
+		echo "###"
+		cd $line
 		git pull origin master
 		echo -en "\033[0m"
 	fi
